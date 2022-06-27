b621eddf1b7fb55119399eb51927d70b719501d2
GrimmzlyTM <lorenzo@tortonmind.com>
2022-06-27 18:22:35 -0400

    feat: optimize OneAway

--- Implementations/Chapter 1 - Arrays and Strings/OneAwayExtensions.cs
+++ Implementations/Chapter 1 - Arrays and Strings/OneAwayExtensions.cs
@@ -12,13 +12,15 @@ public static class OneAwayExtensions {
     public static bool OneAway(this string have, string want) {
         if (Math.Abs(have.Length - want.Length) > 1) return false;
 
+        var shorterString = have.Length > want.Length ? want : have;
+        var primeString = have.Length > want.Length ? have : want;
+        
         var j = 0;
         var editCount = 0;
-        for (int i = 0; i < have.Length; i++) {
-            if ( j > want.Length - 1 || have[i] != want[j]) {
+        for (int i = 0; i < primeString.Length; i++) {
+            if ( j > shorterString.Length - 1 || primeString[i] != shorterString[j]) {
                 editCount++;
-                if (have.Length - i > want.Length - j) continue;
-                if (have.Length - i < want.Length - j) i--;
+                if (primeString.Length - i > shorterString.Length - j) continue;
             }
             j++;
         }
