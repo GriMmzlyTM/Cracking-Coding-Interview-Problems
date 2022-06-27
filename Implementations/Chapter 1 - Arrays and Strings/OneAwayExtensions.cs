d13d2a28d9d0c30012c315a7fc959493428e8a6c
GrimmzlyTM <lorenzo@tortonmind.com>
2022-06-27 18:14:04 -0400

    feat: OneAway problem solution

---
+++ Implementations/Chapter 1 - Arrays and Strings/OneAwayExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Implementations.Chapter_1___Arrays_and_Strings; 
+
+public static class OneAwayExtensions {
+
+    /// <summary>
+    /// O(N) time, where n is largest string length
+    /// O(1) space 
+    /// </summary>
+    /// <param name="have"></param>
+    /// <param name="want"></param>
+    /// <returns></returns>
+    public static bool OneAway(this string have, string want) {
+        if (Math.Abs(have.Length - want.Length) > 1) return false;
+
+        var j = 0;
+        var editCount = 0;
+        for (int i = 0; i < have.Length; i++) {
+            if ( j > want.Length - 1 || have[i] != want[j]) {
+                editCount++;
+                if (have.Length - i > want.Length - j) continue;
+                if (have.Length - i < want.Length - j) i--;
+            }
+            j++;
+        }
+
+        return editCount <= 1;
+    }
+}
---
+++ Tests/Chapter 1 - Arrays and Strings Tests/TestOneAway.cs
@@ -0,0 +1,30 @@
+﻿using Implementations.Chapter_1___Arrays_and_Strings;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+
+namespace TestProject1.Chapter_1___Arrays_and_Strings_Tests; 
+
+public class TestOneAway {
+
+    [Test,
+    TestCase("pale", "ple"),
+    TestCase("pales", "pale"),
+    TestCase("pale", "bale"),
+    TestCase("bake", "bale"),
+    TestCase("harvard", "havard"),
+    TestCase("abcde", "fabcde")]
+    public void OneAway_Should_BeTrue(string a, string b) {
+        Assert.IsTrue(a.OneAway(b));
+    }
+    
+    [Test,
+     TestCase("pale", "bake"),
+     TestCase("pals", "bal"),
+     TestCase("harvard", "havrardi"),
+     TestCase("basket", "basketing"),
+     TestCase("abcdefg", "abdgfg"),
+     TestCase("abcde", "fabdde"),]
+    public void OneAway_Should_BeFalse(string a, string b) {
+        Assert.IsFalse(a.OneAway(b));
+    }
+}
