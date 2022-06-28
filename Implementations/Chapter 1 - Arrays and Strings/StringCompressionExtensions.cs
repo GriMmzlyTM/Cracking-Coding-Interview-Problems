a33bb19575594e3d1b280c8b8b942aa50ebb2ce9
GrimmzlyTM <lorenzo@tortonmind.com>
2022-06-28 14:16:57 -0400

    feat: string compression, stringbuilder

---
+++ Implementations/Chapter 1 - Arrays and Strings/StringCompressionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace Implementations.Chapter_1___Arrays_and_Strings; 
+
+public static class StringCompressionExtensions {
+    public static string CompressString_Brute(this string str) {
+        if (str.Length <= 1) return str;
+
+        var sb = new StringBuilder();
+        
+        var lastChar = str[0];
+        var charCount = 1;
+        
+        for (int i = 1; i < str.Length; i++) {
+            var val = str[i];
+
+            if (lastChar == val) charCount++;
+            else {
+                sb.Append($"{lastChar}{charCount}");
+                lastChar = val;
+                charCount = 1;
+            }
+            
+            if (i == str.Length - 1)
+                sb.Append($"{lastChar}{charCount}");
+        }
+
+        var compressedString = sb.ToString();
+        return compressedString.Length < str.Length ? compressedString : str;
+    }
+}
---
+++ Tests/Chapter 1 - Arrays and Strings Tests/TestStringCompression.cs
@@ -0,0 +1,21 @@
+﻿using Implementations.Chapter_1___Arrays_and_Strings;
+using NUnit.Framework;
+
+namespace TestProject1.Chapter_1___Arrays_and_Strings_Tests; 
+
+public class TestStringCompression {
+
+    [Test,
+    TestCase("aabccccaaa", "a2b1c4a3"),
+    TestCase("abcddDefghijabcdd", "abcddDefghijabcdd"),
+    TestCase("aabbccddeeffgg", "aabbccddeeffgg"),
+    TestCase("aabbccdddeeffgg", "a2b2c2d3e2f2g2"),
+    TestCase("aabbbbc", "a2b4c1"),
+    TestCase("a", "a"),
+    TestCase("aa", "aa"),
+    TestCase("EEE", "E3"),
+    TestCase("", "")]
+    public void StringCompression_Should_Succeed(string given, string expected) {
+        Assert.AreEqual(expected, given.CompressString_Brute());
+    }
+}
